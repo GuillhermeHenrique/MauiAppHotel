90487aaa5aecaba3033c1971619e9ea49ef7043d
GuillhermeHenrique <piresguilherme003@gmail.com>
2024-11-13 16:06:22 -0300

    Parte do back-end

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,13 +1,48 @@
-﻿
+﻿using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Super luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 75.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Single",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+
+        };
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new InterfaceGrafica.ContratacaoHospedagem());
+
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
